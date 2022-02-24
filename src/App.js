fc6bf0b6c397b43da3212827182a08d745bc7baf
NikhilPatil00 <steelenman@gmail.com>
2022-02-24 23:56:39 +0530

    jumplinks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import NET from "vanta/dist/vanta.net.min";
 
-import Navbar from './components/navbar';
+import "./App.css";
+import Navbar from "./components/navbar";
 
 function App() {
-  return (
-    <Navbar />
-  );
+  const [vantaEffect, setVantaEffect] = useState(0);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        NET({
+          el: document.body,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          color: "#f72585",
+          backgroundColor: "#001845",
+          points: 11.0,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
+  return <Navbar />;
 }
 
 export default App;
